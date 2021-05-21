c2157a215515dbb8461f90b9bf56a30262fe6297
sushilk246 <84543942+sushilk246@users.noreply.github.com>
2021-05-21 13:06:41 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -17,13 +17,14 @@ mongoose.connect('mongodb+srv://sk123:sk123@cluster0.vezj4.mongodb.net/dx?retryW
 
 
 
-
+//api for home page
 routes.get('/', async function(req, res) {
       let art = await Article.find();
       res.render('index', { articles: art });
 
 });
 
+//api for showing all post
 routes.get('/showall', async function(req, res) {
       let art = await Article.find();
       res.render('showall', { articles: art });
@@ -31,17 +32,19 @@ routes.get('/showall', async function(req, res) {
 
 });
 
+//api for showing full post
 routes.get('/:id', async (req, res) => {
       const art = await Article.findById(req.params.id);
       if (art == null) res.redirect('/');
       res.render('show', { article: art })
 });
 
+//api for creating new post
 routes.get('/new', function(req, res) {
       res.render('new');
 });
 
-
+//api saving new post in database
 routes.post('/new', async function(req, res) {
       let art=new Article({
                               uid:req.body.idd,
@@ -59,7 +62,8 @@ routes.post('/new', async function(req, res) {
         res.render( 'new');
       }
 });
-////////////////////////////////////
+
+//api for search
 routes.post('/search', async function(req, res) {
       const art=await Article.findOne({uid:req.body.idd});
 
@@ -67,6 +71,7 @@ routes.post('/search', async function(req, res) {
       res.render('show', { article: art })
 });
 
+//api for delete operation
 routes.post('/delete', async function(req, res) {
 
       const art=await Article.findOneAndDelete({uid:req.body.idd});
@@ -75,6 +80,7 @@ routes.post('/delete', async function(req, res) {
       res.render('successfullydeleted')
 });
 
+//api for update operation
 routes.post('/update', async function(req, res) {
 
       const art = await Article.findOne({uid:req.body.idd});
@@ -83,7 +89,7 @@ routes.post('/update', async function(req, res) {
 });
 
 
-///////////////////////////////////////////////////////////////
+// api for update
 routes.put('/:id',async function(req,res){
       let art=await Article.findById(req.params.id);
       art.uid=req.body.idd;
@@ -101,7 +107,8 @@ routes.put('/:id',async function(req,res){
         res.redirect('/');
       }
 });
-///
+
+//api for adding comment
 routes.post('/addcomment', async function(req, res) {
 
       //const art = await Article.findOne({uid:req.body.idd});
@@ -110,6 +117,7 @@ routes.post('/addcomment', async function(req, res) {
       res.render('addcomment', { uid: req.body.idd })
 });
 
+//api for saving newcomment in database
 routes.post('/savecomment', async function(req, res) {
       let art=new Comment({
                               uid:req.body.idd,
@@ -126,6 +134,7 @@ routes.post('/savecomment', async function(req, res) {
       }
 });
 
+//api for fetching all comment from database
 routes.post('/allcomment', async function(req, res) {
       //var =req.body.idd;
       const art = await Comment.find( {uid:req.body.idd} );
